151d9252cd7c6de45aac7c54e07fdc067f55fa8b
Sebastian Kołodziej <sebastiankolodziej87@gmail.com>
2022-11-23 22:07:23 +0100

    finish task 03

--- 03/app.js
+++ 03/app.js
@@ -11,4 +11,19 @@ const buttonSettings = {
         color: '#444'
     },
     text: 'Click me!',
+};
+
+const buttonElement = document.querySelector('.parent-for-button');
+const button = document.createElement('button');
+buttonElement.appendChild(button);
+for(const buttonKey in buttonSettings.attr) {
+    const buttonValue = buttonSettings.attr[buttonKey];
+    button[buttonKey] = buttonValue;
 }
+
+for(const buttonKey in buttonSettings.css) {
+    const value = buttonSettings.css[buttonKey];
+    button.style[buttonKey] = value;
+}
+
+button.innerText = buttonSettings.text;
