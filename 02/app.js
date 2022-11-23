7a88bfc33b44f7639d9017b0476068e83aa7d390
Sebastian Kołodziej <sebastiankolodziej87@gmail.com>
2022-11-23 12:53:43 +0100

    finish task 02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const links = document.querySelectorAll('[data-url]');
+console.log(links);
+
+links.forEach(function(element) {
+    const contentAtt = element.getAttribute('[data-url]');
+    element.setAttribute('href', contentAtt);
+});
