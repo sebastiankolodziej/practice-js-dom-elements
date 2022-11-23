929c2df0e227d22d417669a689bf3e490fd404dd
Sebastian Kołodziej <sebastiankolodziej87@gmail.com>
2022-11-23 11:57:17 +0100

    finis task 01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const element = document.querySelector('.comments__item.comments__item--newest');
+console.log(element);
+if(element) {
+    const elementWithAttribute = element.querySelectorAll('[data-info]');
+    console.log(elementWithAttribute.length);
+}
