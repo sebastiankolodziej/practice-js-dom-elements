050ed195082cafdca01991ff45fbcdf353a7fb78
Sebastian Kołodziej <sebastiankolodziej87@gmail.com>
2022-11-24 13:39:43 +0100

    end of two out of five parts

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,20 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const button = document.createElement('BUTTON');
+if(curr) {
+    const newButton = curr.parentElement;
+    newButton.appendChild(button);
+    button.innerText = 'usuń z koszyka'
+}
+    // end of part one
+
+if(curr) {
+    const newClass = curr.parentElement;
+    for(const child of newClass.children) {
+        child.classList.add('siblings');
+    }
+}
+
+    // end of part two
