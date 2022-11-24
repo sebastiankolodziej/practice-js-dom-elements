17b4d3ca6f37c93b0b7a2ec23ece3a1951e1a4b2
Sebastian Kołodziej <sebastiankolodziej87@gmail.com>
2022-11-24 12:31:45 +0100

    finish task 04

--- 04/app.js
+++ 04/app.js
@@ -2,32 +2,36 @@ console.log('DOM');
 
 const navElement = document.querySelector('nav');
 
-const ulElement = document.createElement('ul');
-const liElement = document.createElement('li');
-const aElement = document.createElement('a');
+// const ulElement = document.createElement('ul');
+// const liElement = document.createElement('li');
+// const aElement = document.createElement('a');
 
-navElement.appendChild(ulElement);
-ulElement.appendChild(liElement);
-liElement.appendChild(aElement);
+// navElement.appendChild(ulElement);
+// ulElement.appendChild(liElement);
+// liElement.appendChild(aElement);
 
-aElement.innerText = 'start';
-aElement.setAttribute('href', '/');
+// aElement.innerText = 'start';
+// aElement.setAttribute('href', '/');
 
-const liElement1 = liElement.cloneNode(true);
-const liElementA = liElement1.querySelector('a');
-liElementA.setAttribute('href', '/gallery');
-liElementA.innerText = 'galeria';
+    // end of first aElement
 
-ulElement.appendChild(liElement1);
+// const liElement1 = liElement.cloneNode(true);
+// const liElementA = liElement1.querySelector('a');
+// liElementA.setAttribute('href', '/gallery');
+// liElementA.innerText = 'galeria';
 
-const liElement2 = liElement.cloneNode(true);
-const liElementA1 = liElement2.querySelector('a');
-liElementA1.setAttribute('href', '/contact');
-liElementA1.innerText = 'kontakt';
+// ulElement.appendChild(liElement1);
 
-ulElement.appendChild(liElement2);
+    // end of second aElement
 
+// const liElement2 = liElement.cloneNode(true);
+// const liElementA1 = liElement2.querySelector('a');
+// liElementA1.setAttribute('href', '/contact');
+// liElementA1.innerText = 'kontakt';
 
+// ulElement.appendChild(liElement2);
+
+    // end of first step menu
 
 // struktura do wykorzystania w pętli
 const menuItems = [
@@ -35,3 +39,17 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const ulElement = document.createElement('ul');
+navElement.appendChild(ulElement);
+
+menuItems.forEach(function(item) {
+    const liElement = document.createElement('li');
+    const aElement = document.createElement('a');
+
+    ulElement.appendChild(liElement);
+    liElement.appendChild(aElement);
+
+    aElement.innerText = item.text;
+    aElement.href = item.url;
+});
