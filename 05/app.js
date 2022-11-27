43b7121507215766c12529e5c4a9a3f689a06dca
Sebastian Kołodziej <sebastiankolodziej87@gmail.com>
2022-11-27 14:09:54 +0100

    finish task DOM

--- 05/app.js
+++ 05/app.js
@@ -2,19 +2,49 @@ console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
 
-const button = document.createElement('BUTTON');
+const button = document.createElement('button');
+const newParentElement = curr.parentElement;
+const newAttr = curr.parentElement.nextElementSibling;
+    
 if(curr) {
-    const newButton = curr.parentElement;
-    newButton.appendChild(button);
-    button.innerText = 'usuń z koszyka'
+    newParentElement.appendChild(button);
+    button.innerText = 'usuń z koszyka';
 }
     // end of part one
 
 if(curr) {
-    const newClass = curr.parentElement;
-    for(const child of newClass.children) {
-        child.classList.add('siblings');
-    }
+    for(const child of newParentElement.children) {
+        if(child !== curr){
+            if(child.classList.add('siblings'));
+    }};
 }
 
     // end of part two
+
+if(curr) {
+    if(newAttr) {
+        newAttr.setAttribute('title', 'nextElementSibling');
+    };
+}
+
+    // end of part three
+
+const pElement = document.createElement('p');
+// pElement.innerText = 'tekst';
+
+if(curr) {
+    const pElement1 = newAttr.nextElementSibling;;
+    const btn = pElement1.lastElementChild;
+    if(pElement1) {
+        pElement1.insertBefore(pElement, btn);
+    };
+}
+
+    // end of part four
+
+const cloneElement = curr.parentElement.parentElement;
+if(curr) {
+    const cloneParent1 = newParentElement.cloneNode(true);
+    cloneElement.insertBefore(cloneParent1, newParentElement);
+}
+// end of part five
